19cdad1b83b5fb10591fb74caf763511a4dfefc0
Christopher Francisco <cjfg00gl3@gmail.com>
2024-11-09 17:18:23 -0600

    UPDATE Program.cs by creating a Main

--- Program.cs
+++ Program.cs
@@ -1,34 +1,45 @@
-// Create XLoyaltyHost
-XLoyaltyHost xLoyaltyHost = new XLoyaltyHost(args, 5115, "secrets.json").BuildHost().RunHost();
-// Setup method that tests endpoint to determine if a user exists
-static async Task<XLoyaltyResponseCode> VerifyUsername(string xUsername)
+public class Program
 {
-    // Create local http client
-    var client = new HttpClient();
-    // Get a request from a response sent and Dispose client
-    var response = await client.GetAsync($"http://localhost:5115/VerifyUsername?username={xUsername}");
-    client.Dispose();
-    // Respond based on the response
-    if (response.IsSuccessStatusCode)
+    public static void Main(string[] args)
     {
-        string responseString = await response.Content.ReadAsStringAsync();
-        XLoyaltyResponseCode responseCode = (XLoyaltyResponseCode)sbyte.Parse(responseString);
-        return responseCode;
+        // Create XLoyaltyHost
+        XLoyaltyHost xLoyaltyHost = new XLoyaltyHost(args, 5115, "secrets.json").BuildHost().RunHost();
+
+        // Custom intro label text
+        string introLabelText = "\nWelcome to X LOYALTY!\nEnter an X account username (alphanumeric)\nto determine if it exists or not!";
+
+        // Create the form builder to build the x loyalty form
+        XLoyaltyFormBuilder form = new XLoyaltyFormBuilder("X LOYALTY", new Size(420, 420))
+            .AddIntroLabel(introLabelText, new Point(0, 0), new Size(420, 60))
+            .AddUsernameTextBox("Enter Username Here!", new Point(105, 75), new Size(210, 180))
+            .AddElonSmilingPictureBox("elonSmiling.jpg", new Point(0, 180), new Size(420, 200))
+            .AddElonFrowningPictureBox("elonFrowning.jpeg", new Point(0, 180), new Size(420, 200))
+            .AddElonGoFYourselfPictureBox("elonFYourself.jpg", new Point(0, 180), new Size(420, 200))
+            .AddSubmitTryAgainButton("Go", new Point(180, 120), new Size(60, 30), VerifyUsername, xLoyaltyHost.GetHost())
+            .Run();
     }
-    // Bad request
-    else
+
+    // Setup method that tests endpoint to determine if a user exists
+    public static async Task<XLoyaltyResponseCode> VerifyUsername(string xUsername)
     {
-        return XLoyaltyResponseCode.Error;
+        // Create local http client
+        using (var client = new HttpClient()) // Using HttpClientFactory or IHttpClientFactory would be more appropriate for production
+        {
+            // Get a request from a response sent
+            var response = await client.GetAsync($"http://localhost:5115/VerifyUsername?username={xUsername}");
+
+            // Respond based on the response
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = await response.Content.ReadAsStringAsync();
+                XLoyaltyResponseCode responseCode = (XLoyaltyResponseCode)sbyte.Parse(responseString);
+                return responseCode;
+            }
+            // Bad request
+            else
+            {
+                return XLoyaltyResponseCode.Error;
+            }
+        }
     }
 }
-// Custom intro label text
-string introLabelText = "\nWelcome to X LOYALTY!\nEnter an X account username (alphanumeric)\n to determine if it exists or not!";
-// Create the form builder to build the x loyalty form
-XLoyaltyFormBuilder form = new XLoyaltyFormBuilder("X LOYALTY", new Size(420, 420))
-.AddIntroLabel(introLabelText, new Point(0, 0), new Size(420, 60))
-.AddUsernameTextBox("Enter Username Here!", new Point(105, 75), new Size(210, 180))
-.AddElonSmilingPictureBox("elonSmiling.jpg", new Point(0, 180), new Size(420, 200))
-.AddElonFrowningPictureBox("elonFrowning.jpeg", new Point(0, 180), new Size(420, 200))
-.AddElonGoFYourselfPictureBox("elonFYourself.jpg", new Point(0, 180), new Size(420, 200))
-.AddSubmitTryAgainButton("Go", new Point(180, 120), new Size(60, 30), VerifyUsername, xLoyaltyHost.GetHost())
-.Run();
